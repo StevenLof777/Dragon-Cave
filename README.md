3d84b112f0b39e82f67dbc03e8db193de44d8eee
Jake Finn <lof.steven@gmail.com>
2022-06-01 13:19:56 -0400

    Finish program and update READEME.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dragon Cave
+
+Simple command line game made in Java.
+
+## Demo
+
+![Dragon Cave demo](Dragon-Cave.gif)
+
+## Authors
+
+- [@StevenLof777](https://github.com/StevenLof777)
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(
+        "You are in a land full of dragons. In front of you, " +
+        "you see two caves. In one cave,\nthe dragon is friendly\n" +
+        "and will share his treasure with you.\n" +
+        "The other dragon is greedy and hungry and will eat you on sight.\n" +
+        "Which cave will you go into? (1 or 2)\n");
+
+        int caveChoice = scanner.nextInt();
+
+        final String msg = caveChoice == 2
+                ? "The dragon has given you treasure"
+                : "The dragon has eaten you";
+
+        System.out.println(msg);
     }
 }

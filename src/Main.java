8cd76e52d9808e04d8230996f626483fa1151abe
Jake Finn <lof.steven@gmail.com>
2022-06-05 16:32:13 -0400

    Add try catch blocks to user input.

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(
-        "You are in a land full of dragons. In front of you, " +
-        "you see two caves. In one cave,\nthe dragon is friendly\n" +
-        "and will share his treasure with you.\n" +
-        "The other dragon is greedy and hungry and will eat you on sight.\n" +
-        "Which cave will you go into? (1 or 2)\n");
+                "You are in a land full of dragons. In front of you, " +
+                        "you see two caves. In one cave,\nthe dragon is friendly\n" +
+                        "and will share his treasure with you.\n" +
+                        "The other dragon is greedy and hungry and will eat you on sight.\n" +
+                        "Which cave will you go into? (1 or 2)\n");
 
-        int caveChoice = scanner.nextInt();
+        int caveChoice = 0;
+        try {
+            caveChoice = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
 
         final String msg = caveChoice == 2
                 ? "The dragon has given you treasure"
